6ad8c95b7cc55975a21228699256f6323b40a78e
Aakanksha <aka.pharande@gmail.com>
2025-02-07 17:24:19 -0500

    Create Virtual Network Interface Card (NIC) for the web server

--- main.tf
+++ main.tf
@@ -105,3 +105,18 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+# Creating VNIC
+
+resource "azurerm_network_interface" "nic" {
+  name                    = "${var.labelPrefix}-A05-NIC"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  network_security_group  = azurerm_network_security_group.nsg.id
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
