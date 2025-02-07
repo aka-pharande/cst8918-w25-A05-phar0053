2756df8374e442e6a30583102174dd5b071f3de8
Aakanksha <aka.pharande@gmail.com>
2025-02-07 17:21:21 -0500

    Create Network Security Group (NSG) with SSH and HTTP rules

--- main.tf
+++ main.tf
@@ -75,4 +75,33 @@ resource "azurerm_subnet" "subnet" {
 
 # Creating a NSG
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-A05-NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 2000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 
