952237aad35f7f20467c180939979847c96d8c48
Aakanksha <aka.pharande@gmail.com>
2025-02-07 17:51:30 -0500

    Create virtual machine with Apache2 web server

--- main.tf
+++ main.tf
@@ -120,3 +120,32 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+# Creating a Virtual Machine
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  custom_data = filebase64("init.sh")
+}
