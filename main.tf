38bff3983511c10b948862e6679edb068dae2d4f
Aakanksha <aka.pharande@gmail.com>
2025-02-07 18:12:31 -0500

    Fix NIC resource and add NSG association

--- main.tf
+++ main.tf
@@ -111,13 +111,20 @@ resource "azurerm_network_interface" "nic" {
   name                    = "${var.labelPrefix}-A05-NIC"
   location                = azurerm_resource_group.rg.location
   resource_group_name     = azurerm_resource_group.rg.name
-  network_security_group  = azurerm_network_security_group.nsg.id
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+}
+
+# Associate NSG with the NIC
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 # Creating a Virtual Machine
